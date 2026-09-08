37e4aae7169bdbc1fb2f5e77ceac2761cddcc555
Alban MARTEL <alban.martel@fms-ea.com>
2026-09-08 12:04:53 +0200

    UPDATE modification des commentaires

--- ecole/models/address.py
+++ ecole/models/address.py
@@ -10,7 +10,12 @@ from typing import Optional
 
 @dataclass
 class Address:
-    """Adresse d'une personne (enseignant ou élève)."""
+    """Adresse d'une personne (enseignant ou élève) :
+    - id          : clé primaire de l'entité persistante
+    - street      : rue de l'adresse
+    - city        : ville
+    - postal_code : code postal
+    """
     id: Optional[int] = field(default=None, init=False)
     street: str
     city: str
