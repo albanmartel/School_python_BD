6338067ac74cbe81261af70a41dbbb5e066892e5
Alban Martel <alban.f.j.martel@laposte.net>
2026-09-09 21:33:18 +0200

    UPDATE: 'ecole_modify_postal_code-type.sql' corriger la parenthèse manquante

--- SQL/ecole_modify_postal_code-type.sql
+++ SQL/ecole_modify_postal_code-type.sql
@@ -1,2 +1,2 @@
 -- Modification du type de la données de code postal de int à varchar(6) permet de gérer plus situation
-ALTER TABLE ecole.address MODIFY COLUMN postal_code VARCHAR6) NOT NULL;
+ALTER TABLE ecole.address MODIFY COLUMN postal_code VARCHAR(6) NOT NULL;
