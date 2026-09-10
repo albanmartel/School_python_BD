18f64c83a7adf686f3ed7867304c15215b52161e
Alban MARTEL <alban.martel@fms-ea.com>
2026-09-10 13:47:46 +0200

    UPDATE 'teacher_dao.py' installation de la méthode update et modification de la méthode read

--- ecole/daos/teacher_dao.py
+++ ecole/daos/teacher_dao.py
@@ -4,7 +4,7 @@
 Classe Dao[Teacher]
 """
 
-from models import address
+from models import address, teacher
 from models.address import Address
 from daos.dao import Dao
 from dataclasses import dataclass
@@ -18,20 +18,20 @@ class TeacherDao(Dao[Teacher]):
     def create(self, teacher: Teacher) -> int:
         """Crée en BD l'entité Teacher correspondant au teacher Teacher
 
-        :param teacher: à créer sous forme d'entité Course en BD
+        :param teacher: à créer sous forme d'entité Teacher BD
         :return: le nombre de lignes modifiées par la requête d'insertion en BD (0 si la création a échouée)
         """
 
         teacher.id = int(self.init_counter("id_teacher", "Max_Id_Teacher", "teacher"))
-        values_param: tuple = (teacher.id, teacher.hiring_date, teacher.id)
-        table_params: list[str] = ["id_teacher", "hiring_date", "id_person"]
+        values_param: tuple = (teacher.id, teacher.hiring_date)
+        table_params: list[str] = ["id_teacher", "hiring_date"]
         table_name: str = "teacher"
 
         return self.insert(table_name, table_params, values_param)
 
     def read(self, id_teacher: int) -> Optional[Teacher]:
-        """Renvoit le cours correspondant à l'entité dont l'id est id_course
-           (ou None s'il n'a pu être trouvé)"""
+        """Renvoit le teacher correspondant à l'entité dont l'id est id_teacher
+                   (ou None s'il n'a pu être trouvé)"""
         course: Optional[Teacher]
 
         table_name: str = "teacher"
@@ -39,3 +39,19 @@ class TeacherDao(Dao[Teacher]):
         id_value: int = id_teacher
 
         return self.read_one(id_name, table_name, id_value)
+
+    def update(self, teacher: Teacher) -> bool:
+        """Met à jour en BD l'entité correspondant à obj, pour y correspondre
+
+        :param obj: objet déjà mis à jour en mémoire
+        :return: True si la mise à jour a pu être réalisée
+        """
+        table_name: str = "teacher"
+        data_dict: dict = {
+            "id_teacher": teacher.id,
+            "hiring_date": teacher.hiring_date,
+        }
+        id_name: str = "id_course"
+        id_value: int = teacher.id
+
+        return self.modify(table_name, data_dict, id_name, id_value)
